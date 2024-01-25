039f8f9949dd66287b56db50d0709191ef893b88
jh66637 <jh66637@users.noreply.github.com>
2024-01-25 11:05:13 +0000

    deploy: 8d97b87a8d612ebb50795951b038204598347fdd

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['mappingdofvector_0',['MappingDoFVector',['../classExaDG_1_1MappingDoFVector.html#a02610a355efa9e0350be9647356e570b',1,'ExaDG::MappingDoFVector']]],
-  ['matrixfreedata_1',['MatrixFreeData',['../structExaDG_1_1MatrixFreeData.html#a31ec14ac4105a57596590fbac09046e7',1,'ExaDG::MatrixFreeData']]]
+  ['mappingdofvector_0',['MappingDoFVector',['../classExaDG_1_1MappingDoFVector.html#a0e945df21afeff8b5fb4aa37c7ed1ca3',1,'ExaDG::MappingDoFVector']]],
+  ['matrixfreedata_1',['MatrixFreeData',['../structExaDG_1_1MatrixFreeData.html#a31ec14ac4105a57596590fbac09046e7',1,'ExaDG::MatrixFreeData']]],
+  ['multigridmappings_2',['MultigridMappings',['../classExaDG_1_1MultigridMappings.html#a12caf180ce7d4acfb717bb7d3805ee41',1,'ExaDG::MultigridMappings::MultigridMappings(std::shared_ptr&lt; dealii::Mapping&lt; dim &gt; &gt; mapping, std::shared_ptr&lt; dealii::Mapping&lt; dim &gt; &gt; mapping_coarse_levels)'],['../classExaDG_1_1MultigridMappings.html#a3862742826dee41822efd2757b5582dc',1,'ExaDG::MultigridMappings::MultigridMappings(std::shared_ptr&lt; MappingDoFVector&lt; dim, Number &gt; &gt; mapping_dof_vector, unsigned int const degree_coarse_mappings)']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['initialize_5fcoarse_5fmappings_0',['initialize_coarse_mappings',['../classExaDG_1_1Grid.html#a084af7e067c53140ba5e32fed294e5c9',1,'ExaDG::Grid']]]
+  ['fill_5fgrid_5fcoordinates_5fvector_0',['fill_grid_coordinates_vector',['../classExaDG_1_1HelpersALE.html#aa625c1a3873b379b11617e954e2a2111',1,'ExaDG::HelpersALE']]]
 ];
