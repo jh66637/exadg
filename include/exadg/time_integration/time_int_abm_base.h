8a4f89bb6019cb0402b743d8240352a523255a3f
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2025-06-08 16:43:42 +0200

    make it work for multiple cell categories

--- include/exadg/time_integration/time_int_abm_base.h
+++ include/exadg/time_integration/time_int_abm_base.h
@@ -218,11 +218,11 @@ private:
 
   void
   do_substep_timestep_solve_1(VectorType &       dst,
-                            VectorType const & src,
-                            double             time,
-                            double             dt,
-                            unsigned int       category,
-                            unsigned int       attached)
+                              VectorType const & src,
+                              double             time,
+                              double             dt,
+                            std::vector<unsigned int> const& categories,
+                              unsigned int       attached)
   {
     auto ts = get_time_step_vector();
     ts[0] *= 0.5;
@@ -230,42 +230,64 @@ private:
     ab_intermediate.update(get_current_order(), true, ts);
 
     do_timestep_predict(dst, src, dt, attached, ab_intermediate);
-    do_timestep_predict(dst, src, dt, category, ab);
-    pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category);
-    prepare_vectors_for_next_timestep(category);
+    for(auto const category : categories)
+    {
+      do_timestep_predict(dst, src, dt, category, ab);
+    }
+    for(auto const category : categories)
+    {
+      pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category);
+    }
+    for(auto const category : categories)
+    {
+      prepare_vectors_for_next_timestep(category);
+    }
   }
 
   void
   do_substep_timestep_solve_2(VectorType &       dst,
-                            VectorType const & src,
-                            VectorType const & src_old,
-                            double             time,
-                            double             dt,
-                            unsigned int       category,
-                            unsigned int       attached)
+                              VectorType const & src,
+                              VectorType const & src_old,
+                              double             time,
+                              double             dt,
+                            std::vector<unsigned int> const& categories,
+                              unsigned int       attached)
   {
     do_timestep_predict(dst, src_old, dt, attached, ab);
-    do_timestep_predict(dst, src, dt, category, ab);
-    pde_operator->evaluate(evaluated_operator_np, dst, time + 2.0 * dt, category);
-    prepare_vectors_for_next_timestep(category);
+    for(auto const category : categories)
+    {
+      do_timestep_predict(dst, src, dt, category, ab);
+    }
+    for(auto const category : categories)
+    {
+      pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category);
+    }
+    for(auto const category : categories)
+    {
+      prepare_vectors_for_next_timestep(category);
+    }
   }
 
 
   void
-do_substep_timestep_solve(VectorType &       dst,
-                          VectorType const & src,
-                          double             time,
-                          double             dt,
-                          unsigned int const category1,
-                          unsigned int const category2
-                          )
+  do_substep_timestep_solve(VectorType &       dst,
+                            VectorType const & src,
+                            double             time,
+                            double             dt,
+                            std::vector<unsigned int> const& categories)
   {
-    do_timestep_predict(dst, src, dt, category1, ab);
-    do_timestep_predict(dst, src, dt, category2, ab);
-    pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category1);
-    pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category2);
-    prepare_vectors_for_next_timestep(category1);
-    prepare_vectors_for_next_timestep(category2);
+    for(auto const category : categories)
+    {
+      do_timestep_predict(dst, src, dt, category, ab);
+    }
+    for(auto const category : categories)
+    {
+      pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category);
+    }
+    for(auto const category : categories)
+    {
+      prepare_vectors_for_next_timestep(category);
+    }
   }
 
 
@@ -273,15 +295,24 @@ do_substep_timestep_solve(VectorType &       dst,
   do_timestep_solve() final
   {
     double const t        = get_time();
-    double const dt_small = std::get<0>(lts_time_step_batches[0]);
-    double const dt_large = 2.0 * dt_small;
+    double const dt_small = get_time_step_size()/4.0;
+    double const dt_medium = 2.0 * dt_small;
+    double const dt_large = 2.0 * dt_medium;
 
     VectorType solution_old = solution;
-    auto & intermediate = prediction;
+    auto &     intermediate = prediction;
+    auto      intermediate2 = prediction;
 
-    do_substep_timestep_solve_1(intermediate, solution_old, t, dt_small, 1, 2);
-    do_substep_timestep_solve_2(solution, intermediate,solution_old, t, dt_small, 1, 2);
-    do_substep_timestep_solve(solution, solution_old, t, dt_large, 2, 3);
+    do_substep_timestep_solve_1(intermediate, solution_old, t, dt_small, {1}, 2);
+    do_substep_timestep_solve_2(solution, intermediate, solution_old, t+dt_small, dt_small, {1}, 2);
+    do_substep_timestep_solve_1(intermediate2, solution_old, t, dt_medium, {2,3},4);
+
+    auto solution_intermediate = solution;
+    do_substep_timestep_solve_1(intermediate, solution_intermediate, t+dt_medium, dt_small, {1}, 2);
+    do_substep_timestep_solve_2(solution, intermediate, solution_intermediate, t+dt_medium+dt_small, dt_small, {1}, 2);
+    do_substep_timestep_solve_2(solution, intermediate2,solution_old, t+dt_medium, dt_medium, {2,3},4);
+
+    do_substep_timestep_solve(solution, solution_old, t, dt_large, {4,5});
   }
 
 
