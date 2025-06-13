864161d33fb3950897020a9b16e9e7d55716cdb6
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2025-06-13 22:14:35 +0200

    not tested: restructure file so we can revert duplication of files

--- include/exadg/time_integration/time_int_ab_lts_base.h
+++ include/exadg/time_integration/time_int_ab_lts_base.h
@@ -47,6 +47,7 @@ public:
                                    unsigned int const        order_,
                                    bool const                start_with_low_order_,
                                    bool const                adaptive_time_stepping_,
+                                   bool const                local_time_stepping_,
                                    RestartData const &       restart_data_,
                                    MPI_Comm const &          mpi_comm_,
                                    bool const                is_test_)
@@ -63,7 +64,8 @@ public:
       // order of predictor can be chosen one below order of corrector
       ab(order_ - 1, start_with_low_order_),
       am(order_, start_with_low_order_),
-      vec_evaluated_operators(order_ - 1)
+      vec_evaluated_operators(order_ - 1),
+      local_time_stepping(local_time_stepping_)
   {
     AssertThrow(order_ >= 1,
                 dealii::ExcMessage("Oder of ABM time integrator has to be at least 1."));
@@ -130,15 +132,22 @@ private:
     }
     else // start with high order
     {
-      AssertThrow(false, dealii::ExcMessage("not yet implemented"));
-      // fill evaluated operators
-      // VectorType temp_sol;
-      // pde_operator->initialize_dof_vector(temp_sol);
-      // for(unsigned int i = 0; i < vec_evaluated_operators.size(); ++i)
-      // {
-      //   pde_operator->prescribe_initial_conditions(temp_sol, get_previous_time(i));
-      //   pde_operator->evaluate(vec_evaluated_operators[i], temp_sol, get_previous_time(i));
-      // }
+      if(!local_time_stepping)
+      {
+        // fill evaluated operators
+        VectorType temp_sol;
+        pde_operator->initialize_dof_vector(temp_sol);
+        for(unsigned int i = 0; i < vec_evaluated_operators.size(); ++i)
+        {
+          pde_operator->prescribe_initial_conditions(temp_sol, get_previous_time(i));
+          pde_operator->evaluate(vec_evaluated_operators[i], temp_sol, get_previous_time(i));
+        }
+      }
+      else
+      {
+        // TODO:
+        AssertThrow(false, dealii::ExcMessage("not yet implemented"));
+      }
     }
   }
 
@@ -147,64 +156,61 @@ private:
   {
   }
 
-  // TODO:
-  // void
-  // do_timestep_predict()
-  // {
-  // dealii::Timer timer;
-  // timer.restart();
-  //
-  // predrict_solution(prediction, solution, vec_evaluated_operators);
-  //
-  // timer_tree->insert({"Timeloop", "Adams-Bashforth-Moulton"}, timer.wall_time());
-  // }
-
-  // TODO:
-  //  void
-  //  do_timestep_correct()
-  //  {
-  //    dealii::Timer timer;
-  //    timer.restart();
-  //
-  //    // evaluate operator given the predicted solution
-  //    pde_operator->evaluate(evaluated_operator_np, prediction, get_next_time());
-  //    // correct solution
-  //    correct_solution(solution, evaluated_operator_np, vec_evaluated_operators);
-  //    // correct operator by evaluating operator with correct solution
-  //    pde_operator->evaluate(evaluated_operator_np, solution, get_next_time());
-  //
-  //    // write output
-  //    if(this->print_solver_info() and not(this->is_test))
-  //    {
-  //      pcout << std::endl << "Adams-Bashforth-Moulton:";
-  //      print_wall_time(pcout, timer.wall_time());
-  //    }
-  //
-  //    timer_tree->insert({"Timeloop", "Adams-Bashforth-Moulton"}, timer.wall_time());
-  //  }
-
-  // TODO:
-
   void
-  do_timestep_predict(VectorType &                      p,
-                      VectorType const &                s,
-                      double const                      dt,
-                      dealii::types::material_id        cell_category,
-                      ABTimeIntegratorConstants const & ab_in)
+  do_timestep_predict()
   {
-    predrict_solution(p, s, vec_evaluated_operators, dt, cell_category, ab_in);
+    dealii::Timer timer;
+    timer.restart();
+
+    predrict_solution(prediction, solution, vec_evaluated_operators);
+
+    timer_tree->insert({"Timeloop", "Adams-Bashforth-Moulton"}, timer.wall_time());
   }
+
   void
-  do_timestep_correct(VectorType &               s,
-                      VectorType const &         p,
-                      double                     dt,
-                      double                     next_time,
-                      dealii::types::material_id cell_category)
+  do_timestep_correct()
+  {
+    dealii::Timer timer;
+    timer.restart();
+
+    // evaluate operator given the predicted solution
+    pde_operator->evaluate(evaluated_operator_np, prediction, get_next_time());
+    // correct solution
+    correct_solution(solution, evaluated_operator_np, vec_evaluated_operators);
+    // correct operator by evaluating operator with correct solution
+    pde_operator->evaluate(evaluated_operator_np, solution, get_next_time());
+
+    // write output
+    if(this->print_solver_info() and not(this->is_test))
+    {
+      pcout << std::endl << "Adams-Bashforth-Moulton:";
+      print_wall_time(pcout, timer.wall_time());
+    }
+
+    timer_tree->insert({"Timeloop", "Adams-Bashforth-Moulton"}, timer.wall_time());
+  }
+
+  void
+  do_lts_timestep_predict(VectorType &                      p,
+                          VectorType const &                s,
+                          double const                      dt,
+                          dealii::types::material_id        cell_category,
+                          ABTimeIntegratorConstants const & ab_in)
+  {
+    predrict_solution_lts(p, s, vec_evaluated_operators, dt, cell_category, ab_in);
+  }
+
+  void
+  do_lts_timestep_correct(VectorType &               s,
+                          VectorType const &         p,
+                          double                     dt,
+                          double                     next_time,
+                          dealii::types::material_id cell_category)
   {
     // evaluate operator given the predicted solution
     pde_operator->evaluate(evaluated_operator_np, p, next_time, cell_category);
     // correct solution
-    correct_solution(s, evaluated_operator_np, vec_evaluated_operators, dt, cell_category);
+    correct_solution_lts(s, evaluated_operator_np, vec_evaluated_operators, dt, cell_category);
     // correct operator by evaluating operator with correct solution
     pde_operator->evaluate(evaluated_operator_np, s, next_time, cell_category);
   }
@@ -217,24 +223,22 @@ private:
   };
 
   void
-  do_substep_timestep_solve_1(VectorType &       dst,
-                              VectorType const &                src,
-                              double                            time,
-                              double                            dt,
-                              std::vector<  dealii::types::material_id> const & categories,
-                             dealii::types::material_id const  attached = dealii::numbers::invalid_material_id)
+  do_lts_sub_timestep_solve(VectorType &                                    dst,
+                            VectorType const &                              src,
+                            VectorType const &                              src_old,
+                            double                                          time,
+                            double                                          dt,
+                            std::vector<dealii::types::material_id> const & categories,
+                            dealii::types::material_id const                attached,
+                            ABTimeIntegratorConstants                       ab_attached)
   {
     if(attached != dealii::numbers::invalid_material_id)
     {
-    auto ts = get_time_step_vector();
-    ts[0] *= 0.5;
-    auto ab_intermediate = ab;
-    ab_intermediate.update(get_current_order(), true, ts);
-    do_timestep_predict(dst, src, dt, attached, ab_intermediate);
+      do_lts_timestep_predict(dst, src_old, dt, attached, ab_attached);
     }
     for(auto const category : categories)
     {
-      do_timestep_predict(dst, src, dt, category, ab);
+      do_lts_timestep_predict(dst, src, dt, category, ab);
     }
     for(auto const category : categories)
     {
@@ -242,73 +246,114 @@ private:
     }
     for(auto const category : categories)
     {
-      prepare_vectors_for_next_timestep(category);
+      prepare_vectors_for_next_timestep_lts(category);
     }
   }
 
   void
-  do_substep_timestep_solve_2(VectorType &                      dst,
-                              VectorType const &                src,
-                              VectorType const &                src_old,
-                              double                            time,
-                              double                            dt,
-                              std::vector<  dealii::types::material_id> const & categories,
-                             dealii::types::material_id const  attached )
+  do_lts_sub_timesteps_recursively(VectorType &                       dst,
+                                   VectorType const &                 src,
+                                   double                             t,
+                                   double const                       dt,
+                                   unsigned int                       index,
+                                   std::vector<CellBatchInfo> const & infos,
+                                   ABTimeIntegratorConstants          ab_intermediate)
   {
-    do_timestep_predict(dst, src_old, dt, attached, ab);
-    for(auto const category : categories)
+    if(index > 0)
     {
-      do_timestep_predict(dst, src, dt, category, ab);
-    }
-    for(auto const category : categories)
-    {
-      pde_operator->evaluate(evaluated_operator_np, dst, time + dt, category);
-    }
-    for(auto const category : categories)
-    {
-      prepare_vectors_for_next_timestep(category);
-    }
-  }
+      auto intermediate = src;
 
-  void do_subtimesteps_recursively(VectorType& dst, VectorType const& src, double t, double const dt, unsigned int index, std::vector<CellBatchInfo>const&infos )
-  {
-    if(index>0)
-    {
-     auto intermediate = src;
-     do_subtimesteps_recursively(intermediate,src,t,0.5*dt,index-1,infos);
-     do_substep_timestep_solve_1(intermediate, src, t, dt, infos[index].cell_categories, infos[index].attached_cell_category);
-     do_subtimesteps_recursively(dst,intermediate,t+dt,0.5*dt,index-1,infos);
-     do_substep_timestep_solve_2(dst, intermediate,src, t+dt, dt, infos[index].cell_categories, infos[index].attached_cell_category);
+      do_subtimesteps_recursively(
+        intermediate, src, t, 0.5 * dt, index - 1, infos, ab_intermediate);
+
+      do_lts_sub_timestep_solve(intermediate,
+                                src,
+                                src,
+                                t,
+                                dt,
+                                infos[index].cell_categories,
+                                infos[index].attached_cell_category,
+                                ab_intermediate);
+
+      do_subtimesteps_recursively(
+        dst, intermediate, t + dt, 0.5 * dt, index - 1, infos, ab_intermediate);
+
+      do_lts_sub_timestep_solve(dst,
+                                intermediate,
+                                src,
+                                t + dt,
+                                dt,
+                                infos[index].cell_categories,
+                                infos[index].attached_cell_category,
+                                ab);
     }
     else
     {
       auto intermediate = src;
-      do_substep_timestep_solve_1(intermediate, src, t, dt, infos[index].cell_categories, infos[index].attached_cell_category);
-      do_substep_timestep_solve_2(dst, intermediate,src, t+dt, dt, infos[index].cell_categories, infos[index].attached_cell_category);
+      do_lts_sub_timestep_solve(intermediate,
+                                src,
+                                src,
+                                t,
+                                dt,
+                                infos[index].cell_categories,
+                                infos[index].attached_cell_category,
+                                ab_intermediate);
+
+      do_lts_sub_timestep_solve(dst,
+                                intermediate,
+                                src,
+                                t + dt,
+                                dt,
+                                infos[index].cell_categories,
+                                infos[index].attached_cell_category,
+                                ab);
     }
   }
 
   void
   do_timestep_solve() final
   {
-     double const t         = get_time();
-     double const dt         = get_time_step_size();
+    if(!local_time_stepping)
+    {
+      do_timestep_predict();
+      do_timestep_correct();
+    }
+    else
+    {
+      double const t  = get_time();
+      double const dt = get_time_step_size();
 
-     VectorType solution_old  = solution;
+      VectorType solution_old = solution;
 
-     std::vector<CellBatchInfo> infos;
-     infos.push_back({get_time_step_size() / 4.0,{1},2});
-     infos.push_back({get_time_step_size() / 2.0,{2,3},4});
-     infos.push_back({get_time_step_size() / 1.0,{4,5},dealii::numbers::invalid_material_id});
-     std::sort(infos.begin(), infos.end(), [](auto const&a, auto const&b){return a.dt<b.dt;});
+      std::vector<CellBatchInfo> infos;
+      infos.push_back({get_time_step_size() / 4.0, {1}, 2});
+      infos.push_back({get_time_step_size() / 2.0, {2, 3}, 4});
+      infos.push_back({get_time_step_size() / 1.0, {4, 5}, dealii::numbers::invalid_material_id});
+      std::sort(infos.begin(),
+                infos.end(),
+                [](auto const & a, auto const & b) { return a.dt < b.dt; });
 
-    do_subtimesteps_recursively(solution,solution_old,t, 0.5*dt, infos.size()-2,infos);
-    do_substep_timestep_solve_1(solution, solution_old, t, dt, infos.back().cell_categories, infos.back().attached_cell_category);
+      auto ts = get_time_step_vector();
+      ts[0] *= 0.5;
+      auto ab_intermediate = ab;
+      ab_intermediate.update(get_current_order(), true, ts);
+
+      do_lts_sub_timesteps_recursively(
+        solution, solution_old, t, 0.5 * dt, infos.size() - 2, infos, ab_intermediate);
+
+      do_lts_sub_timestep_solve(solution,
+                                solution_old,
+                                solution_old,
+                                t,
+                                dt,
+                                infos.back().cell_categories,
+                                dealii::numbers::invalid_material_id,
+                                ab);
+    }
   }
 
-
   void
-  prepare_vectors_for_next_timestep(dealii::types::material_id cell_category)
+  prepare_vectors_for_next_timestep_lts(dealii::types::material_id cell_category)
   {
     if(vec_evaluated_operators.size() > 0)
     {
@@ -321,18 +366,35 @@ private:
       pde_operator->copy_dofs_of_cell_category(vec_evaluated_operators[0],
                                                evaluated_operator_np,
                                                cell_category);
-
-      // push_back(vec_evaluated_operators);
-      // std::swap(vec_evaluated_operators_small[0], evaluated_operator_np);
     }
   }
 
   void
   correct_solution(VectorType &                    dst,
                    VectorType const &              op_np,
-                   std::vector<VectorType> const & ops,
-                   double                          dt,
-                   dealii::types::material_id      cell_category) const
+                   std::vector<VectorType> const & ops) const
+  {
+    dst.add(static_cast<Number>(get_time_step_size() * am.get_gamma0()), op_np);
+    for(unsigned int i = 0; i < this->am.get_order() - 1; ++i)
+      dst.add(static_cast<Number>(get_time_step_size() * am.get_alpha(i)), ops[i]);
+  }
+
+  void
+  predrict_solution(VectorType &                    dst,
+                    VectorType const &              src,
+                    std::vector<VectorType> const & ops) const
+  {
+    dst = src;
+    for(unsigned int i = 0; i < this->ab.get_order(); ++i)
+      dst.add(static_cast<Number>(get_time_step_size() * ab.get_alpha(i)), ops[i]);
+  }
+
+  void
+  correct_solution_lts(VectorType &                    dst,
+                       VectorType const &              op_np,
+                       std::vector<VectorType> const & ops,
+                       double                          dt,
+                       dealii::types::material_id      cell_category) const
   {
     pde_operator->add_dofs_of_cell_category(dst,
                                             static_cast<Number>(dt * am.get_gamma0()),
@@ -346,19 +408,15 @@ private:
                                               ops[i],
                                               cell_category);
     }
-
-    // dst.add(static_cast<Number>(dt * am.get_gamma0()), op_np);
-    // for(unsigned int i = 0; i < this->am.get_order() - 1; ++i)
-    //   dst.add(static_cast<Number>(dt * am.get_alpha(i)), ops[i]);
   }
 
   void
-  predrict_solution(VectorType &                      dst,
-                    VectorType const &                src,
-                    std::vector<VectorType> const &   ops,
-                    double                            dt,
-                    dealii::types::material_id        cell_category,
-                    ABTimeIntegratorConstants const & ab_in) const
+  predrict_solution_lts(VectorType &                      dst,
+                        VectorType const &                src,
+                        std::vector<VectorType> const &   ops,
+                        double                            dt,
+                        dealii::types::material_id        cell_category,
+                        ABTimeIntegratorConstants const & ab_in) const
   {
     pde_operator->copy_dofs_of_cell_category(dst, src, cell_category);
     for(unsigned int i = 0; i < ab_in.get_order(); ++i)
@@ -368,22 +426,17 @@ private:
                                               ops[i],
                                               cell_category);
     }
-
-    // dst = src;
-    // for(unsigned int i = 0; i < this->ab.get_order(); ++i)
-    //   dst.add(static_cast<Number>(dt * ab.get_alpha(i)), ops[i]);
   }
 
-
-  // TODO:
   void
   prepare_vectors_for_next_timestep() final
   {
-    // if(vec_evaluated_operators_large.size() > 0)
-    // {
-    //   push_back(vec_evaluated_operators_large);
-    //   std::swap(vec_evaluated_operators_large[0], evaluated_operator_np_large);
-    // }
+    // for local timestepping this is already done during do_timestep_solve()
+    if(vec_evaluated_operators.size() > 0 && !local_time_stepping)
+    {
+      push_back(vec_evaluated_operators);
+      std::swap(vec_evaluated_operators[0], evaluated_operator_np);
+    }
   }
 
   void
@@ -406,17 +459,6 @@ private:
     AssertThrow(false, dealii::ExcMessage("Steady not implemented."));
   }
 
-public:
-  void
-  set_lts_time_step_batches(
-    std::vector<std::tuple<double,
-                           std::vector<unsigned int>,
-                           std::vector<unsigned int>,
-                           std::vector<unsigned int>>> lts_time_step_batches_in)
-  {
-    lts_time_step_batches = std::move(lts_time_step_batches_in);
-  }
-
 private:
   // spatial pde operator
   std::shared_ptr<Operator> pde_operator;
@@ -434,11 +476,7 @@ private:
   VectorType              evaluated_operator_np;
   std::vector<VectorType> vec_evaluated_operators;
 
-  std::vector<std::tuple<double,
-                         std::vector<unsigned int>,
-                         std::vector<unsigned int>,
-                         std::vector<unsigned int>>>
-    lts_time_step_batches;
+  bool const local_time_stepping;
 };
 
 } // namespace ExaDG
