58f503370b0e5e8c43a6499975910a50047efd21
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2026-03-05 20:48:45 +0100

    implement feedback term

--- include/exadg/aero_acoustic/volume_coupling.h
+++ include/exadg/aero_acoustic/volume_coupling.h
@@ -85,9 +85,12 @@ public:
 
     source_term_calculator.setup(fluid_solver_in->pde_operator->get_matrix_free(), data);
 
-    //TODO:
-    // FeedbackTermCalculatorData<dim> data_feedback;
-    // feedback_term_calculator.setup(acoustic_solver_in->pde_operator->get_matrix_free(), data_feedback);
+
+    FeedbackTermCalculatorData data_feedback;
+    data_feedback.dof_index  = acoustic_solver_in->pde_operator->get_dof_index_velocity();
+    data_feedback.quad_index = acoustic_solver_in->pde_operator->get_quad_index_velocity();
+
+    feedback_term_calculator.setup(acoustic_solver_in->pde_operator->get_matrix_free(), data_feedback);
   }
 
   void
@@ -134,10 +137,9 @@ public:
   {
     non_nested_grid_transfer.interpolate(convection_acoustic,fluid_solver->time_integrator->get_velocity());
 
-    //TODO:
-    // feedback_term_calculator.evaluate_integrate(feedback_term_acoustic,
-    //                                       convection_acoustic,
-    //                                       acoustic_solver->time_integrator->get_velocity());
+    feedback_term_calculator.evaluate_integrate(feedback_term_acoustic,
+                                          convection_acoustic,
+                                          acoustic_solver->time_integrator->get_velocity());
 
     feedback_term_fluid = 0.0;
     non_nested_grid_transfer.prolongate_and_add(feedback_term_fluid, feedback_term_acoustic);
@@ -164,6 +166,8 @@ private:
 
   // Class that knows how to compute the source term
   SourceTermCalculator<dim, Number> source_term_calculator;
+
+  FeedbackTermCalculator<dim, Number> feedback_term_calculator;
 
   // Aeroacoustic source term defined on the acoustic mesh
   VectorType source_term_acoustic;
