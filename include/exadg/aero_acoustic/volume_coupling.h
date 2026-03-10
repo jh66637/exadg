355f5c76b4eb764bf1cafd5f1f44d1246fed27b0
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2026-03-10 20:15:51 +0100

    clean up

--- include/exadg/aero_acoustic/volume_coupling.h
+++ include/exadg/aero_acoustic/volume_coupling.h
@@ -86,11 +86,13 @@ public:
 
 
     FeedbackTermCalculatorData data_feedback;
-    data_feedback.dof_index  = acoustic_solver_in->pde_operator->get_dof_index_velocity();
-    data_feedback.quad_index = acoustic_solver_in->pde_operator->get_quad_index_velocity();
+    data_feedback.dof_index          = fluid_solver_in->pde_operator->get_dof_index_velocity();
+    data_feedback.dof_index_acoustic = acoustic_solver_in->pde_operator->get_dof_index_velocity();
+    data_feedback.quad_index = fluid_solver_in->pde_operator->get_quad_index_velocity_standard();
     data_feedback.density    = parameters_in.density;
 
-    feedback_term_calculator.setup(acoustic_solver_in->pde_operator->get_matrix_free(),
+    feedback_term_calculator.setup(fluid_solver_in->pde_operator->get_matrix_free(),
+                                   acoustic_solver_in->pde_operator->get_matrix_free(),
                                    data_feedback);
   }
 
