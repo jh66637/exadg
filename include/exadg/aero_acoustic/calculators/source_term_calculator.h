4d2bd5d1e3a4f2c6f60bc126b77458803af17c26
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2026-03-10 20:15:15 +0100

    clean up

--- include/exadg/aero_acoustic/calculators/source_term_calculator.h
+++ include/exadg/aero_acoustic/calculators/source_term_calculator.h
@@ -97,6 +97,7 @@ struct FeedbackTermCalculatorData
 {
   unsigned int dof_index;
   unsigned int quad_index;
+  unsigned int dof_index_acoustic;
   double       density;
 };
 
@@ -106,6 +107,8 @@ class FeedbackTermCalculator
   using This                 = FeedbackTermCalculator<dim, Number>;
   using VectorType           = dealii::LinearAlgebra::distributed::Vector<Number>;
   using CellIntegratorVector = CellIntegrator<dim, dim, Number>;
+  using RemoteCellIntegratorVector =
+    dealii::FERemoteEvaluation<dim, dim, dealii::VectorizedArray<Number>>;
 
 
 public:
