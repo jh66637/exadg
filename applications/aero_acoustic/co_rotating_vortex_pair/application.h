1e8be01951ef35ddca9d94dcb78b2c1f4ca42663
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2026-03-12 20:15:43 +0100

    remove parameters that are overwriiten

--- applications/aero_acoustic/co_rotating_vortex_pair/application.h
+++ applications/aero_acoustic/co_rotating_vortex_pair/application.h
@@ -486,20 +486,6 @@ public:
     // PROJECTION METHODS
 
     // pressure Poisson equation
-    this->param.solver_data_pressure_poisson =
-      SolverData(1e4, 1.e-12, 1.e-6, LinearSolver::CG, 100);
-    this->param.preconditioner_pressure_poisson      = PreconditionerPressurePoisson::Multigrid;
-    this->param.multigrid_data_pressure_poisson.type = MultigridType::cphMG;
-    this->param.multigrid_data_pressure_poisson.smoother_data.smoother =
-      MultigridSmoother::Chebyshev;
-    this->param.multigrid_data_pressure_poisson.smoother_data.preconditioner =
-      PreconditionerSmoother::PointJacobi;
-    this->param.multigrid_data_pressure_poisson.coarse_problem.solver =
-      MultigridCoarseGridSolver::Chebyshev;
-    this->param.multigrid_data_pressure_poisson.coarse_problem.preconditioner =
-      MultigridCoarseGridPreconditioner::PointJacobi;
-
-    // pressure Poisson equation
     this->param.solver_data_pressure_poisson = SolverData(1000, ABS_TOL, REL_TOL, LinearSolver::CG);
     this->param.preconditioner_pressure_poisson      = PreconditionerPressurePoisson::Multigrid;
     this->param.multigrid_data_pressure_poisson.type = MultigridType::cphMG;
